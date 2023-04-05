80723b18b055446ceb249a05d980df1e89ebb2be
Luis Rachope <luis.chiasso@hotmail.com>
2023-04-05 17:48:56 -0300

    Primeira melhorias do SOLID no projeto uso do DRY e atribuição de responsabilidade (single source of truth)

---
+++ src/Alura.LeilaoOnline.WebApp/Dados/leilaoDao.cs
@@ -0,0 +1,41 @@
+﻿using Alura.LeilaoOnline.WebApp.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Alura.LeilaoOnline.WebApp.Dados
+{
+    public class LeilaoDao
+    {
+        AppDbContext _context;
+
+        public LeilaoDao()
+        {
+            _context = new AppDbContext();
+        }
+
+        public IEnumerable<Categoria> BuscarCategorias() => _context.Categorias.ToList();
+        public IEnumerable<Leilao> BuscarLeiloes() => _context.Leiloes.Include(l => l.Categoria).ToList();
+        public Leilao BuscarPorId(int id) => _context.Leiloes.First(l => l.Id == id);
+
+        public void Incluir(Leilao leilao)
+        {
+            _context.Leiloes.Add(leilao);
+            _context.SaveChangesAsync();
+        }
+
+        public void Alterar(Leilao leilao)
+        {
+            _context.Leiloes.Update(leilao);
+            _context.SaveChangesAsync();
+        }
+
+        public void Excluir(Leilao leilao)
+        {
+            _context.Leiloes.Remove(leilao);
+            _context.SaveChangesAsync();
+        }
+    }
+}
